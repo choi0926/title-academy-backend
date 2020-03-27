d47122949ce452a695a8caa2415c186f3f7cfc26
choihosung <ghtjd753753@gmail.com>
2020-03-28 00:11:05 +0900

    초기설정

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { ApolloServer } from 'apollo-server';
+import resolvers from './graphql/resolvers';
+import typeDefs from './graphql/typeDefs';
+
+// ApolloServer는 스키마와 리졸버가 반드시 필요함
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+
+
+const port = 4000;
+// listen 함수로 웹 서버 실행
+server.listen({ port: process.env.PORT || port }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
